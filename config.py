edac87cb383f8b6b673b4fd61ac680efbc1f2093
chintu704203 <133212763+chintu704203@users.noreply.github.com>
2024-05-22 22:57:32 +0530

    Update config.py
    Signed-off-by: chintu704203 <133212763+chintu704203@users.noreply.github.com>

--- config.py
+++ config.py
@@ -13,15 +13,15 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
+PING_IMG = getenv("PING_IMG", "https://graph.org/file/22ac22e03c4fdab0dc3a1.jpg")
+START_IMG = getenv("START_IMG", "https://graph.org/file/5d9e61b1ddeace3af1263.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Gleam_moon")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Gleam_moon")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
 
 
-FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
+FAILED = "https://graph.org/file/22ac22e03c4fdab0dc3a1.jpg"
