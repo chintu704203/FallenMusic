1ef8f9360c6920adf24199d3425d98c5a328c130
chintu704203 <133212763+chintu704203@users.noreply.github.com>
2024-06-06 15:16:56 +0530

    Update config.py
    Signed-off-by: chintu704203 <133212763+chintu704203@users.noreply.github.com>

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://graph.org/file/22ac22e03c4fdab0dc3a1.jpg")
+PING_IMG = getenv("PING_IMG", "https://graph.org/file/5d9e61b1ddeace3af1263.jpg")
 START_IMG = getenv("START_IMG", "https://graph.org/file/5d9e61b1ddeace3af1263.jpg")
 
 SESSION = getenv("SESSION", None)
@@ -24,4 +24,4 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Gleam_moon")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
 
 
-FAILED = "https://graph.org/file/22ac22e03c4fdab0dc3a1.jpg"
+FAILED = "https://graph.org/file/5d9e61b1ddeace3af1263.jpg"
